d61af71aa52559e2acade9856e7d6defdb1d4ea6
BurlakovIvan <burlakovid@inbox.ru>
2022-06-24 20:44:35 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,21 @@
+package ru.job4j.generics;
+
+public class Animal {
+    private String name;
+
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Object{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
