3dcd1b5fe435620a9f30305af1931202eb588f14
BurlakovIvan <burlakovid@inbox.ru>
2022-06-28 20:39:25 +0300

    6. Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -17,7 +17,7 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public void add(T value) {
-        Node<T> node = new Node<T>(value, null);
+        Node<T> node = new Node<>(value, null);
         if (head == null) {
             head = node;
             return;
@@ -42,13 +42,30 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public T addFirst(T value) {
-        head = new Node<T>(value, head);
+        head = new Node<>(value, head);
         return head.value;
+    }
+
+    public boolean revert() {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Node<T> previous = null;
+        Node<T> current = head;
+        while (current.next != null) {
+            Node<T> next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = current;
+        head.next = previous;
+        return true;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<T>() {
+        return new Iterator<>() {
             Node<T> node = head;
 
             @Override
