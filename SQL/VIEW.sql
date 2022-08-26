11664a1fb835ead27e919629db2e1532aa60c0de
BurlakovIvan <burlakovid@inbox.ru>
2022-08-27 00:01:15 +0300

    1. Представления [#504792]

---
+++ SQL/VIEW.sql
@@ -0,0 +1,66 @@
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS author;
+DROP TABLE IF EXISTS genre;
+
+CREATE TABLE author (
+    author_id serial PRIMARY KEY,
+    name_author VARCHAR(50)
+);
+
+INSERT INTO author (name_author)
+VALUES ('Булгаков М.А.'),
+       ('Достоевский Ф.М.'),
+       ('Есенин С.А.'),
+       ('Пастернак Б.Л.');
+
+CREATE TABLE genre (
+    genre_id serial PRIMARY KEY,
+    name_genre varchar(30)
+);
+
+INSERT INTO genre (name_genre)
+VALUES ('Роман'),
+       ('Поэзия'),
+       ('Приключения');
+
+CREATE TABLE book (
+    book_id serial PRIMARY KEY,
+    title VARCHAR(50),
+    author_id INT REFERENCES author (author_id),
+    genre_id int REFERENCES genre (genre_id),
+    price DECIMAL(8, 2),
+    amount INT
+);
+
+INSERT INTO book (title, author_id, genre_id, price, amount)
+VALUES ('Мастер и Маргарита', 1, 1, 670.99, 3),
+       ('Белая гвардия ', 1, 1, 540.50, 5),
+       ('Идиот', 2, 1, 460.00, 10),
+       ('Братья Карамазовы', 2, 1, 799.01, 3),
+       ('Игрок', 2, 1, 480.50, 10),
+       ('Стихотворения и поэмы', 3, 2, 650.00, 15),
+       ('Черный человек', 3, 2, 570.20, 6),
+       ('Лирика', 4, 2, 518.99, 2);
+       
+SELECT title, name_author, name_genre, price, amount
+    FROM author 
+    INNER JOIN book ON author.author_id = book.author_id
+    INNER JOIN genre ON  book.genre_id = genre.genre_id
+GROUP BY title, name_author, name_genre, price, amount, genre.genre_id
+HAVING genre.genre_id IN
+         (SELECT query_in_1.genre_id
+          FROM 
+              (SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+               )query_in_1
+          INNER JOIN 
+              ( SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+                ORDER BY sum_amount DESC
+                LIMIT 1
+               ) query_in_2
+          ON query_in_1.sum_amount= query_in_2.sum_amount
+         )
+ORDER BY title;
