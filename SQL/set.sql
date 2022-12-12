3c216b88969fc336bb0415faf636b2e1c65264c0
BurlakovIvan <burlakovid@inbox.ru>
2022-12-13 00:45:41 +0300

    Операторы множества в SQL

---
+++ SQL/set.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS movie (
+    id SERIAL PRIMARY KEY,
+    name text,
+    director text
+);
+
+CREATE TABLE IF NOT EXISTS book (
+    id SERIAL PRIMARY KEY,
+    title text,
+    author text
+);
+
+INSERT INTO movie (name, director)
+VALUES ('Марсианин', 'Ридли Скотт'),
+       ('Матрица', 'Братья Вачовски'),
+       ('Властелин колец', 'Питер Джексон'),
+       ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
+       ('Железный человек', 'Джон Фавро');
+
+INSERT INTO book (title, author)
+VALUES ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+       ('Властелин колец', 'Джон Толкин'),
+       ('1984', 'Джордж Оруэлл'),
+       ('Марсианин', 'Энди Уир'),
+       ('Божественная комедия', 'Данте Алигьери');
+
+ -- названия всех фильмов, которые сняты по книге
+
+ SELECT name FROM movie
+ INTERSECT
+ SELECT title FROM book;
+
+ --все названия книг, у которых нет экранизации
+
+ SELECT title FROM book
+ EXCEPT
+ SELECT name FROM movie;
+
+--все уникальные названия произведений из таблиц movie и book
+
+ SELECT title AS name FROM book
+ UNION
+ SELECT name FROM movie
+ ORDER BY name;
